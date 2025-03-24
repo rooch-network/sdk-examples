4538bb3c8b01401213a88e093a70728e9cb0ba5b
Joe Chen <geometryolife@gmail.com>
2025-03-25 03:08:04 +0800

    update getStates

--- ts-sdk/getStates/src/index.ts
+++ ts-sdk/getStates/src/index.ts
@@ -8,12 +8,16 @@ async function main() {
   });
 
   const states = await client.getStates({
-    accessPath: '/object/0x3',
+    // accessPath: '/object/0x3',
+    accessPath: '/object/0xc87604825b720bc01434686db6001c0f69122856edc446fa6c367834fc8b21e9006abb3c49e638d9c4ad3edb394d46c007bfe985648d627d3aee52f90eab8000',
     stateOption: {
       decode: true
     }
   });
   console.log(states);
+  console.log('\nState Root:', states[0].state_root);
+  console.log('\nDecoded Value:', states[0].decoded_value);
+
 }
 
 main()
